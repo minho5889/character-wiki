659f0c0244b4b944689d6dd822e89ab58496f699
Minho lee <mlee246@myseneca.ca>
2022-02-03 11:24:11 -0500

    xx

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,55 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Cards from '../components/Cards/Cards';
-import InputGroup from '../components/Filters/Category/InputGroup';
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card/Card";
+import InputGroup from "../components/Filter/category/InputGroup";
 
 const Episodes = () => {
-  
-  let [id, setID] = useState(1)
-  let [info, setInfo] = useState([])
-  let {results, setResults} = useState([]) 
-  let {air_date, name} = info 
-  let api = `https://rickandmortyapi.com/api/episode/${id}`
-  
+  let [results, setResults] = React.useState([]);
+  let [info, setInfo] = useState([]);
+  let { air_date, episode, name } = info;
+  let [id, setID] = useState(1);
+
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
 
   useEffect(() => {
-
-    (async function() {
-      let data = await fetch(api).then(res=>res.json())
-      setInfo(data)
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
 
       let a = await Promise.all(
-        data.characters.map(character => {
-          return fetch(character).then((res) => res.json())
+        data.characters.map((x) => {
+          return fetch(x).then((res) => res.json());
         })
       );
       setResults(a);
-    })()
-  }, [api])
+    })();
+  }, [api]);
+
   return (
     <div className="container">
-      <div className="row mb-4">
-        <h1 className="text-center">
-          Episode :{" "}
-          <span className="text-primary" {name ===""? "Unknown" : name}></span>
+      <div className="row mb-3">
+        <h1 className="text-center mb-3">
+          Episode name :{" "}
+          <span className="text-primary">{name === "" ? "Unknown" : name}</span>
         </h1>
-        <h5 className="text-center">Air Date {air_date===""? "Unknown" : air_date}
+        <h5 className="text-center">
+          Air Date: {air_date === "" ? "Unknown" : air_date}
         </h5>
       </div>
       <div className="row">
-        <div className="col-3">
-          <h4 className="text-center mb-4">
-            PICK EPISODES
-          </h4>
-          <InputGroup setID={setID} name="Episode" total={51} /> 
+        <div className="col-lg-3 col-12 mb-4">
+          <h4 className="text-center mb-4">Pick Episode</h4>
+          <InputGroup name="Episode" changeID={setID} total={51} />
         </div>
-        <div className="col-8">
+        <div className="col-lg-8 col-12">
           <div className="row">
-            <Cards results={results} />
+            <Card page="/episodes/" results={results} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Episodes
+export default Episodes;
